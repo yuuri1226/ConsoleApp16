d376a7fe50194219ab9c4d185520537950bf1e7f
yuuri <yuuri@occ.co.jp>
2021-05-12 13:18:03 +0900

    edit-main

--- ConsoleApp16/Program.cs
+++ ConsoleApp16/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp16
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
